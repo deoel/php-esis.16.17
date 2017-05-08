20aa194791006c4461e75ad66f0a50f597e21586
deoel <mwanakahambo@gmail.com>
2017-05-08 10:24:36 +0200

    include

---
+++ lesson4.php
@@ -0,0 +1,13 @@
+<?php
+
+	echo '<h1>Karibu</h1>';
+	//include
+	
+
+	require_once ('page1.php');
+	require_once 'page1.php';
+	require_once 'page1.php';
+	
+	
+	echo '<h3>Merci pour votre visite</h3>';
+?>
---
+++ page1.php
@@ -0,0 +1,22 @@
+<?php
+
+	echo '
+		<strong>Annonce 1</strong>
+		<p>
+			Mark Zuck vient en RDC bientot<br/>
+		</p>
+		<strong>Annonce 2</strong>
+		<p>
+			Je suis sur Github<br/>
+		</p>
+		<strong>Annonce 3</strong>
+		<p>
+			Moïse sera le nouveau président<br/>
+		</p>
+	';
+	
+	
+	
+	
+
+?>

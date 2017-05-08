e754059699ee018e49e9132112981da0a31834d0
deoel <mwanakahambo@gmail.com>
2017-05-08 09:28:28 +0200

    test4

--- test.php
+++ test.php
@@ -17,7 +17,7 @@
 	
 	//test3
 	
-	function tintin($a) {
+	/*function tintin($a) {
 		$c = array();
 		foreach ($a as $b) {
 			$c[] = $a[$b];
@@ -30,6 +30,30 @@
 	
 	echo "<pre>";
 	print_r($s);
+	echo "</pre>";*/
+	
+
+	//test3
+	
+	function milou($a) {
+		$c = array();
+		foreach ($a as $b) {
+			array_unshift($c, $b);
+			$e = array_pop($c);
+			array_push($c, $e);
+		}
+		
+		return $c;
+	}
+
+	$d = array(5,3,4,1,0,2);
+	$s = milou($d);
+	$l = array_flip($s);
+	
+	echo "<pre>";
+	print_r($s);
+	print_r($l);
 	echo "</pre>";
 	
+
 ?>

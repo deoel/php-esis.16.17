dd67048346b9c870a1b1b2cc2cd4c2b09d69e9da
Tshilay <14bn017@esisalama.org>
2017-06-26 22:43:40 +0200

    tshilay interro de ratrapage

--- attributionFonctions/__add_tache.php
+++ attributionFonctions/__add_tache.php
@@ -1,28 +1,26 @@
-<?php
-
+<?php 
 	require_once('tache.class.php');
 	require_once('tache.dao.php');
-				
-				
 
-	if(isset($_POST['idagent']) and isset($_POST['description']) and !empty($_POST['datedebut']) and !empty($_POST['datefin'])) 
-	{
-		$tache = new Tache(0, $_POST['description'], $_POST['datedebut'], $_POST['datefin'], $_POST['idagent']);
-		$tdao = new TacheDAO();
-		
-		if($tdao->AddTache($tache) == true)
-		{
-			header('Location: taches.php');
+
+	if (isset($_POST['description'], $_POST['datedebut'], $_POST['datefin'], $_POST['idagent']) 
+		and !empty($_POST['description']) and !empty($_POST['datedebut']) and !empty($_POST['datefin']) and !empty($_POST['idagent'])) {
+
+	$tache = new Tache(0, $_POST['description'], $_POST['datedebut'], $_POST['datefin'], $_POST['idagent']);
+	$tdao = new TacheDAO();
+	$lt = $tdao->getAllTache();
+	$find = false;
+	foreach ($lt as $t) {
+		if ($tache->getDescription() == $t->getDescription()) {
+			$find = true;
 		}
-		else
-		{
-			header('Location: taches.php');
-		}
-		
-	} 
-	else 
-	{
-		echo 'Remplissez Tous Les Champs';
 	}
-
-?>
+	if (!$find) {
+		$tdao->add($tache);
+		header('Location: taches.php');
+	}
+		
+	}else{
+		echo 'problems ';
+	}
+ ?>
--- attributionFonctions/del_tache.php
+++ attributionFonctions/del_tache.php
@@ -1,38 +1,12 @@
-<!doctype>
-<html>
-	<head>
-		<title>Attribution des fonctions</title>
-		<meta charset="utf-8" />
-		<link rel="stylesheet" href="style.css" />
-	</head>
-	<body>
-		<?php include_once('head.php'); ?>
+<?php 
+	require_once('tache.dao.php');
 
-		<div>
-			
-			<?php
-				require_once('tache.class.php');
-				require_once('tache.dao.php');
+	if (isset($_GET['id']) and !empty($_GET['id'])) {
 
-				
-				$tdao = new TacheDAO();
-				
-				if(isset($_GET['id']))
-				{
-					$id = $_GET['id'];
-					
-					if($tdao->delTache($id) === true)
-					{
-						echo "<p>Tache Supprimee Avec Succes</p>";
-					}
-					else
-					{
-						echo "<p>Tache Non Trouvee</p>";
-					}
-				}
-			?>
-		</div>
-		
-		<?php include_once('foot.php'); ?>
-	</body>
-</html>
+		$tdao = new TacheDAO();
+		$tdao->supprimer($_GET['id']);
+		header('Location: taches.php');
+	}else{
+		echo 'error on page del_tache.php';
+	}
+ ?>

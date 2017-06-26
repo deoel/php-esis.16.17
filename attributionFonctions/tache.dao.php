b8e4aa889ee1d5517d1aaf4d52cbf4a8f1e4852a
NASSORLORD <nassorlord@gmail.com>
2017-06-26 17:10:03 +0200

    fin du codage

--- attributionFonctions/tache.dao.php
+++ attributionFonctions/tache.dao.php
@@ -20,6 +20,64 @@
 			
 			return $tabTache;
 		}
+
+		function nombreTache($id){
+			try {
+				$req = $this->bdd->prepare('SELECT COUNT(*) FROM tache where idagent = :id');
+			} catch (Exception $e) {
+				echo $e->getMessage();
+			}
+			
+			$req->execute(array(
+				'id'=>$id
+				));
+			$result = $req->fetch()[0];
+			return $result;
+		}
+		function supprimer($id){
+			try {
+				$req = $this->bdd->prepare('DELETE FROM tache where id = :id');
+			} catch (Exception $e) {
+				echo $e->getMessage();
+			}
+			
+			$req->execute(array(
+				'id'=>$id
+				));
+		}
+
+		function modifyTache($id, $description, $debut, $fin, $idagent){
+			try {
+				$req = $this->bdd->prepare('UPDATE tache SET description =:description, datedebut = :datedebut, datefin = :datefin, idagent = :idagent
+					where id = :id');
+			} catch (Exception $e) {
+				echo $e->getMessage();
+			}
+			
+			$req->execute(array(
+				'id'=>$id,
+				'description'=>$description,
+				'datedebut'=>$debut,
+				'datefin'=>$fin,
+				'idagent'=>$idagent
+				));
+		}
+
+		function add(Tache $tache){
+			try {
+				$req = $this->bdd->prepare('INSERT INTO tache (description, datedebut, datefin, idagent) VALUES
+				(:description, :datedebut, :datefin, :idagent)');
+			} catch (Exception $e) {
+				echo $e->getMessage();
+			}
+			
+			$req->execute(array(
+				'description'=>$tache->getDescription(),
+				'datedebut'=>$tache->getDatedebut(),
+				'datefin'=>$tache->getDatefin(),
+				'idagent'=>$tache->getIdagent()
+				));
+		}
 		
 	}
 
